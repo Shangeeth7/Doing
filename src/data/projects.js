beba9129c8d58d6200e3d629e920d831a69a01e8
SHANGEETH <shanshangeeth@gmail.com>
2022-12-01 17:45:57 +0530

    nn

---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+// TODO Add a couple lines about each project
+const data = [
+  {
+    title: 'Zula Minds',
+    link: 'https://www.zulaminds.com/',
+
+    image: '/images/projects/zulaminds.jpg',
+    date: '2021-11-20',
+    desc: 'An array of educational aids to engage and nurture young minds ',
+  },
+  {
+    title: 'Online Bike Service Booking',
+    subtitle: 'Select a mechanic of your Motorcycle type and his experiance',
+    link: 'https://effulgent-donut-d412c4.netlify.app/about-us',
+    image: '/images/projects/bikeservice.jpg',
+    date: '2022-11-28',
+    desc:
+      'You can Select a mechanic based on his Experience and Motorcycle Type. '
+      + '--Apply for a Job as a Mechanic. --'
+      + 'Login Required  ',
+  },
+  {
+    title: 'Movies list with Games',
+
+    link: '',
+    image: '/images/projects/movie-games.jpg',
+    date: '2022-06-20',
+    desc: 'Netflix alike website with games  ',
+  },
+];
+
+export default data;
